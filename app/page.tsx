81f01e8388c2edcdec4fedbf49e06a74a4268eb4
Starmagichat <154031802+starmagichat@users.noreply.github.com>
2024-09-17 11:45:03 +0000

    細かい説明をつけたのでtable-fetchは一旦完了でauthブランチに移行します

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,3 @@
-// 'use client'
-
-// import { useState, useEffect } from 'react'
-// import { tableFetch } from '@/app/utils/tableFetch'
-
-// interface Todo {
-//     id: number
-//     title: string
-//     // 他の必要なフィールドを追加
-// }
-
-// export default function Todos() {
-//     const [todos, setTodos] = useState<Todo[]>([])
-
-//     useEffect(() => {
-//         async function loadTodos() {
-//             const tabledFetch = await tableFetch()
-//             setTodos(tabledFetch)
-//         }
-//         loadTodos()
-//     }, [])
-
-//     return (
-//         <ul>
-//             {todos.map((todo) => (
-//                 <li key={todo.id}>{todo.title}</li>
-//             ))}
-//         </ul>
-//     )
-// }
-
 'use client'
 import { useState, useEffect } from 'react'
 import { tableFetch } from '@/app/utils/tableFetch'
--- app/utils/tableFetch.ts
+++ app/utils/tableFetch.ts
@@ -1,12 +1,3 @@
-// import { supabase } from '@/app/utils/supabase'
-
-// export const tableFetch = async () => {
-//     const todos = await supabase.from("storys").select("*")
-//     return todos.data
-
-// }
-
-
 import { supabase } from '@/app/utils/supabase'
 
 export const tableFetch = async () => {
